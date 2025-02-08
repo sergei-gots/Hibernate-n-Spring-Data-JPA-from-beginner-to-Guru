67e17b3d354ad4d1ad2a179de97a9b36f5c752e8
sergei <sergei.gots@yahoo.com>
2025-02-08 20:15:53 +0300

    L34. H2 ↔ MySQL Compatibility Mode

--- src/test/java/guru/springframework/sdjpaintro/SpringBootDataJpaSliceTest.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootDataJpaSliceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @ComponentScan("guru.springframework.sdjpaintro.bootstrap")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 public class SpringBootDataJpaSliceTest {
 
