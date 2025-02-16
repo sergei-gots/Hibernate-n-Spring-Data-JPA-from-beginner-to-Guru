6ef56d01a53373d7345c0e95d3b7e7f25fc72331
sergei <sergei.gots@yahoo.com>
2025-02-17 00:05:59 +0300

    L53. strategy=GenerationType.IDENTITY - auto_increment PK

--- src/main/java/guru/springframework/sdjpaintro/domain/Author.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @Entity
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
