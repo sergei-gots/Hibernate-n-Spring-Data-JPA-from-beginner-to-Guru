3124a13468e33f3d581af46d454da1e5e65ed02b
sergei <sergei.gots@yahoo.com>
2025-02-17 15:07:08 +0300

    L57.Assignment-4 Write Unit Tests

--- src/main/java/guru/springframework/sdjpaintro/repositories/BookRepository.java
+++ src/main/java/guru/springframework/sdjpaintro/repositories/BookRepository.java
@@ -3,8 +3,10 @@ package guru.springframework.sdjpaintro.repositories;
 import guru.springframework.sdjpaintro.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 /**
  * Created by goc on 24/05/2024
  */
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
--- src/test/java/guru/springframework/sdjpaintro/SpringBootDataJpaSliceTest.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootDataJpaSliceTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.domain.Author;
 import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.AuthorRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -26,17 +28,27 @@ public class SpringBootDataJpaSliceTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    AuthorRepository authorRepository;
+
     @Commit
     @Order(1)
     @Test
-    void testJpaTestSlice() {
+    void testJpaBookTestSlice() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
 
-        bookRepository.save(new Book("Spring Boot Slice Test in action. Bootstrap for data", "ISBN-Lesson-29", "MySelf", null));
+        String isbn = "ISBN-Lesson-29";
+        Book savedBook = bookRepository.save(new Book("Spring Boot Slice Test in action. Bootstrap for data", isbn, "MySelf", null));
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+        assertThat(savedBook.getId()).isNotNull();
+
+        Book gotBook = bookRepository.getReferenceById(savedBook.getId());
+        assertThat(gotBook).isNotNull();
+        assertThat(gotBook.getId()).isEqualTo(savedBook.getId());
+        assertThat(gotBook.getIsbn()).isEqualTo(isbn);
     }
 
     @Order(2)
@@ -44,5 +56,26 @@ public class SpringBootDataJpaSliceTest {
     void testJpaTestSliceTransaction() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(3);
+    }
+
+    @Test
+    void testJpaAuthorTestSplice() {
+
+        String firstName = "Jaques";
+        Author author = new Author(firstName, "Derrida");
+
+        long countBefore = authorRepository.count();
+        Author savedAuthor = authorRepository.save(author);
+        long countAfter = authorRepository.count();
+
+        assertThat(countAfter).isEqualTo(countBefore+1);
+        assertThat(savedAuthor).isNotNull();
+        assertThat(savedAuthor.getId()).isNotNull();
+        assertThat(savedAuthor.getFirstName()).isEqualTo(firstName);
+
+        Author gotAuthor = authorRepository.getReferenceById(savedAuthor.getId());
+
+        assertThat(gotAuthor).isEqualTo(savedAuthor);
+        assertThat(gotAuthor.getFirstName()).isEqualTo(firstName);
     }
 }
